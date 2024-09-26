73e01760be641088363c171f7fd4c580fb937dae
João Lee <joaumlimaum@gmail.com>
2024-09-26 16:07:50 -0300

    Adiciona semântica ao HTML - remove erros se houver

--- scripts/authenticacao-teste.js
+++ scripts/authenticacao-teste.js
@@ -1,10 +1,43 @@
 const formLogin = document.querySelector("#form-login");
+const inputContainer = document.querySelector(".input-group-entrar");
+// console.log(inputContainer.children);
 
 const loginInfo = localStorage.getItem("auth");
 const parsedLoginInfo = JSON.parse(loginInfo);
 
 const getUsers = () => JSON.parse(localStorage.getItem("users"));
 const usersList = getUsers();
+
+const hasErrorMessage = (element) =>
+  element.querySelector(".erro-autenticacao-login");
+
+const showErrorMessage = (errorType) => {
+  if (!errorType) {
+    const existentError = hasErrorMessage(inputContainer);
+    existentError && existentError.remove();
+    return;
+  }
+
+  const errorSpan = document.createElement("span");
+  const [inputEmail, inputPassword] = inputContainer.children;
+  const existentError = hasErrorMessage(inputPassword);
+  console.log("não tem erro");
+
+  const { error } = errorType;
+  switch (error) {
+    case "password":
+      errorSpan.innerText = "A senha está incorreta.";
+      errorSpan.classList.add("erro-autenticacao-login");
+      inputPassword.appendChild(errorSpan);
+      break;
+    case "email":
+      errorSpan.innerText = "O e-mail está incorreto.";
+      errorSpan.classList.add("erro-autenticacao-login");
+      inputEmail.appendChild(errorSpan);
+    default:
+      break;
+  }
+};
 
 function getAuthentication(email, password) {
   for (let i = 0; i < usersList.length; i++) {
@@ -14,14 +47,17 @@ function getAuthentication(email, password) {
         console.log("tá tudo certo");
         return;
       }
+      console.log("senha tá errada");
       return { error: "password" };
     }
+    console.log("email não cadastrado");
     return { error: "email" };
   }
 }
-const disableButton = (button) => {
-  button.innerText = "";
-  button.disabled = true;
+
+const controlButtonDisablement = (button) => {
+  button.disabled = button.disabled ? false : true;
+  button.innerText = !button.disabled ? "Entrar" : "";
 };
 
 const createAndAppendLoadingSpinner = (button) => {
@@ -32,12 +68,18 @@ const createAndAppendLoadingSpinner = (button) => {
 
 formLogin.addEventListener("submit", (ev) => {
   ev.preventDefault();
+  console.log("remove");
+  showErrorMessage();
   const [email, password, submitButton] = ev.target;
-  disableButton(submitButton);
-  console.log(submitButton);
+  controlButtonDisablement(submitButton);
   createAndAppendLoadingSpinner(submitButton);
   setTimeout(() => {
     const hasAuthError = getAuthentication(email, password);
-    console.log(hasAuthError);
+    if (hasAuthError) {
+      controlButtonDisablement(submitButton);
+      showErrorMessage(hasAuthError);
+      return;
+    }
+    // showErrorMessage();
   }, 2000);
 });

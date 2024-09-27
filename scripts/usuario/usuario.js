424937dcd105f2abcc318b75867c474ef26d2242
João Lee <joaumlimaum@gmail.com>
2024-09-27 00:26:09 -0300

    Alterações diversas
    Muda estilo do botão de logout, refatora código e exibição de informações e cria funcionalidade de logout

--- scripts/usuario/usuario.js
+++ scripts/usuario/usuario.js
@@ -1,20 +1,37 @@
 const userInfoList = document.querySelector(".informacao-lista");
 const principalContainer = document.getElementsByTagName("main")[0];
-document.addEventListener("DOMContentLoaded", () => {
-  const userInfo = JSON.parse(localStorage.getItem("auth"));
-  const username = userInfo.nomeCompleto.split(" ")[0];
+const logoutButton = document.querySelector("#logout-botao");
+
+const infoName = ["Nome completo", "CPF", "Endereço", "E-mail"];
+const { auth, senha, ...userInfo } = JSON.parse(localStorage.getItem("auth"));
+
+const createGreeting = (userInfos) => {
+  const { nomeCompleto } = userInfos;
   const greeting = document.createElement("h2");
-  greeting.innerText = "Olá, ";
-  greeting.innerHTML += `<span>${username}</span>`;
+  greeting.innerText = "Olá,";
+  greeting.innerHTML += ` <span>${nomeCompleto.split(" ")[0]}</span>!`;
   greeting.id = "boas-vindas";
   principalContainer.prepend(greeting);
+};
 
-  for (const info of Object.values(userInfo)) {
-    if (info === true) continue;
+document.addEventListener("DOMContentLoaded", () => {
+  const infoList = Object.values(userInfo);
+  for (let i = 0; i < infoList.length; i++) {
     const listInfoItem = document.createElement("li");
-    listInfoItem.innerText = info;
-    console.log(info);
+    listInfoItem.innerText = `${infoName[i]}: ${infoList[i]}`;
     userInfoList.append(listInfoItem);
   }
-  console.log(userInfo);
+  createGreeting(userInfo);
+});
+
+logoutButton.addEventListener("click", (ev) => {
+  logoutButton.innerText = "Saindo...";
+  logoutButton.disabled = true;
+  logoutButton.classList.add("loging-out");
+  setTimeout(() => {
+    // remove a pessoa logada do
+    // localStorage.removeItem("auth");
+    // vai para home
+    // window.location.assign("/pages/home/home.html");
+  }, 2000);
 });

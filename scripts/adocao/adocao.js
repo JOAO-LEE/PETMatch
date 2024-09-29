2180ce7fb7320db45eb76ca6f7281ac063a9d233
gabisennaa <gabrielasenna74@gmail.com>
2024-09-28 23:30:25 -0300

    renderização de pet selecionado

---
+++ scripts/adocao/adocao.js
@@ -0,0 +1,25 @@
+const containerInformacao = document.querySelector(".informacoes-pet")
+const containerListaDeInformacoes = document.querySelector(".lista-informacoes")
+
+window.addEventListener("DOMContentLoaded", ()=>{
+    const petSelecionado = localStorage.getItem("adocao")
+    const petSelecionadoConvertido = JSON.parse(petSelecionado)
+
+    const imagemPet = document.createElement("img")
+    imagemPet.src = petSelecionadoConvertido.imagem
+    containerInformacao.appendChild(imagemPet)
+
+    containerListaDeInformacoes.innerHTML = `
+        <li>${petSelecionadoConvertido.nome}</li>
+        <li>${petSelecionadoConvertido.peso}</li>
+        <li>${petSelecionadoConvertido.idadeAproximada}</li>
+        <li>${petSelecionadoConvertido.microchip}</li>
+        <li>${petSelecionadoConvertido.especie}</li>
+        <li>${petSelecionadoConvertido.porte}</li>
+        <li>${petSelecionadoConvertido.raca}</li>
+        <li>${petSelecionadoConvertido.caracteristica}</li>
+        <li>${petSelecionadoConvertido.local}</li>
+        <li>${petSelecionadoConvertido.sobre}</li>
+    
+    `
+})

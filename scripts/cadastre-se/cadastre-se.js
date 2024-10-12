4d0bebde5a54decd78060980b48027718ed442b5
João Lee <joaumlimaum@gmail.com>
2024-10-11 21:19:36 -0300

    Validações concluídas e cadastro de usuário

--- scripts/cadastre-se/cadastre-se.js
+++ scripts/cadastre-se/cadastre-se.js
@@ -1,49 +1,47 @@
+import { pictureList } from "../common/constants/pictureList.js";
+import { authenticateUser } from "../common/auth/authentication.js";
+// import { hasErrorMessage } from "../common/auth/login.js";
 const formularioCadastro = document.querySelector("#cadastro");
 const labelCPF = document.querySelector("#etiquetaCPF");
 const labelNome = document.querySelector("#etiquetaNome");
 const labelEmail = document.querySelector("#etiquetaEmail");
 
-// import { pictureList } from "../common/constants/pictureList.js";
-// import { authenticateUser } from "../common/auth/authentication.js";
-// import { hasErrorMessage } from "../common/auth/login.js";
 const regexNome = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
 const regexCPF = /^\d{11}$/;
 const regexEmail = /^[a-z0-9.]+@[a-z0-9]+.[a-z]{2,}$/i;
 
-localStorage.setItem(
-  "usuarios",
-  JSON.stringify([
-    {
-      nomeCompleto: "Katia Silva Veloso",
-      cpf: "12345678910",
-      endereco: "Rua das Garças, n92, Maria da Graça, Rio de Janeiro",
-      email: "katiaveloso@gmail.com",
-      senha: "123456",
-    },
-    {
-      nomeCompleto: "Sergio Buzaranho",
-      cpf: "14991823765",
-      endereco: "Rua das Garças, n92, Maria da Graça, Rio de Janeiro",
-      email: "sergiobuzaranho@gmail.com",
-      senha: "123456",
-    },
-    {
-      nomeCompleto: "João Vitor Ferreira Lima",
-      cpf: "98765432100",
-      endereco: "Rua das Garças, n92, Maria da Graça, Rio de Janeiro",
-      email: "joaovitor_123@gmail.com",
-      senha: "123456",
-    },
-  ])
-);
+const usuarios = localStorage.getItem("usuarios");
+
+// localStorage.setItem(
+//   "usuarios",
+//   JSON.stringify([
+//     {
+//       nomeCompleto: "Katia Silva Veloso",
+//       cpf: "12345678910",
+//       endereco: "Rua das Garças, n92, Maria da Graça, Rio de Janeiro",
+//       email: "katiaveloso@gmail.com",
+//       senha: "123456",
+//     },
+//     {
+//       nomeCompleto: "Sergio Buzaranho",
+//       cpf: "14991823765",
+//       endereco: "Rua das Garças, n92, Maria da Graça, Rio de Janeiro",
+//       email: "sergiobuzaranho@gmail.com",
+//       senha: "123456",
+//     },
+//     {
+//       nomeCompleto: "João Vitor Ferreira Lima",
+//       cpf: "98765432100",
+//       endereco: "Rua das Garças, n92, Maria da Graça, Rio de Janeiro",
+//       email: "joaovitor_123@gmail.com",
+//       senha: "123456",
+//     },
+//   ])
+// );
 formularioCadastro.addEventListener("submit", async (e) => {
   e.preventDefault();
-  // console.log("oi");
   const formHasError = e.target.querySelector(".erro-autenticacao-login");
   const [...formLabels] = e.target.getElementsByTagName("label");
-
-  // formLabels.some
-
   if (formHasError) {
     formLabels.forEach((label) => {
       const hasErrorMessage = label.classList.contains("error-label");
@@ -84,34 +82,39 @@ formularioCadastro.addEventListener("submit", async (e) => {
   );
 
   if (hasRegisteredUser) {
+    const errorContainer = document.querySelector(".erro-campo-cpf-email");
+    const errorMessage = document.createElement("p");
+    errorMessage.innerText = hasRegisteredUser.error;
+    errorMessage.classList.add("mensagem-erro");
+    errorContainer.appendChild(errorMessage);
+    return;
   }
 
-  // const profilePic = await getRandomPic();
+  const profilePic = await getRandomPic();
 
-  // const novoUsuario = {
-  //   nomeCompleto: inputNome.value,
-  //   endereco: inputEndereco.value,
-  //   CPF: inputCPF.value,
-  //   email: inputEmail.value,
-  //   senha: inputSenha.value,
-  //   imagem: profilePic,
-  // };
+  const novoUsuario = {
+    nomeCompleto: inputNome.value,
+    endereco: inputEndereco.value,
+    CPF: inputCPF.value,
+    email: inputEmail.value,
+    senha: inputSenha.value,
+    imagem: profilePic,
+    agendamentos: [],
+  };
 
-  // localStorage.setItem(
-  //   "usuarios",
-  //   JSON.stringify([...usuarios, { ...novoUsuario }])
-  // );
-  // authenticateUser(novoUsuario);
-  // window.location.assign("/pages/usuario/usuario.html");
+  localStorage.setItem(
+    "usuarios",
+    JSON.stringify([...usuarios, { ...novoUsuario }])
+  );
+  authenticateUser(novoUsuario);
+  window.location.assign("/pages/usuario/usuario.html");
 });
 
 const getUsers = (cpf, email) => {
-  console.log(typeof cpf);
   const users = JSON.parse(localStorage.getItem("usuarios"));
   for (let index = 0; index < users.length; index++) {
     const user = users[index];
     if (user.cpf === cpf) {
-      // console.log("cpf é igual po");
       return { error: "CPF já cadastrado" };
     } else if (user.email === email) {
       return { error: "Email já cadastrado" };
